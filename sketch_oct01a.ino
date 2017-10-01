306b55fd5a85d786530adf446db4ecf5b9fe8e21
Nicholas100 <32439902+Nicholas100@users.noreply.github.com>
2017-10-01 18:49:40 -0300

    Add files via upload

---
+++ sketch_oct01a.ino
@@ -0,0 +1,23 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+//Programa de controle ao motor.
+  bool motor_a=6;
+ bool motor_b=3;
+ 
+ void setup() {
+ Serial.begin(9600);
+ pinMode(motor_a,OUTPUT);
+}
+void loop() {
+ if(Serial.available()){
+ readBluetooth = Serial.read();//Valor lido na Serial será guardado na variável readBluetooth
+ if(readBluetooth == 'a'){
+  }
+ if(readBluetooth == 'a'){
+ digitalWrite(motor_a,HIGH); 
+  }
+  if(readBluetooth == 'b'){
+  digitalWrite(motor_b,LOW);
+    }
+ 
